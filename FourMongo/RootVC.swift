332625ce7570f39635065c7d8ca2c40df37bc88d
MoMoWan <momo@www.gz.10086.cn>
2016-03-15 00:04:19 +0800

    添加了必要的滚动页面

--- FourMongo/RootVC.swift
+++ FourMongo/RootVC.swift
@@ -12,22 +12,49 @@ import UIKit
 let screenWidth = UIScreen.mainScreen().bounds.width
 let screenHeight = UIScreen.mainScreen().bounds.height
 
-class RootVC: UIViewController {
+let scrollViewHeight = screenHeight - 64
+
+class RootVC: UINavigationController {
     
-    
-    //底层ScrollView容器
-    @IBOutlet weak var scrollView: UIScrollView!
-    //首页宽度
-    @IBOutlet weak var profilePageWidth: NSLayoutConstraint!
-    @IBOutlet weak var goodsPageWidth: NSLayoutConstraint!
+    var mainScrollView:UIScrollView?
+    //在该控制器下添加三个容器页面,高度需要去除导航栏64
+    func addThreeSubContainer(){
+        mainScrollView = UIScrollView(frame: CGRect(x: 0, y: 64, width: screenWidth, height: screenHeight - 64))
+        mainScrollView?.contentSize = CGSize(width: 3*screenWidth, height: screenHeight - 64)
+        mainScrollView?.pagingEnabled = true
+        mainScrollView?.showsHorizontalScrollIndicator = false
+        mainScrollView?.showsVerticalScrollIndicator = false
+        
+        //1、添加第一个页面
+        var profilePageView = UITableView(frame: CGRect(x: 0, y: 0, width: screenWidth, height: scrollViewHeight))
+        mainScrollView?.addSubview(profilePageView)
+        profilePageView.backgroundColor = UIColor.redColor() //
+        
+        //2、添加第二个页面（商品详细信息页面）
+        var goodsInfoView = UIScrollView(frame: CGRect(x: screenWidth, y: 64, width: screenWidth, height: screenHeight - 64))
+        goodsInfoView.contentSize = CGSize(width: 2*screenWidth, height: screenHeight - 64)
+        mainScrollView?.addSubview(goodsInfoView)
+        
+        goodsInfoView.pagingEnabled = true
+        goodsInfoView.showsHorizontalScrollIndicator = false
+        goodsInfoView.showsVerticalScrollIndicator = false
+        
+        goodsInfoView.backgroundColor = UIColor.yellowColor() //
+        
+        //3、添加第三个页面（商品搜索）
+        var searchPageView = UITableView(frame: CGRect(x: 2*screenWidth, y: 0, width: screenWidth, height: scrollViewHeight))
+        mainScrollView?.addSubview(searchPageView)
+        searchPageView.backgroundColor = UIColor.darkTextColor() //
+        
+        self.view.addSubview(mainScrollView!)
+    }
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        //横向可以滚动三个页面，高度需要去除导航栏64
-        scrollView.contentSize = CGSize(width: screenWidth*3, height: screenHeight - 64)
-        profilePageWidth.constant = screenWidth
-        goodsPageWidth.constant = screenWidth
+        self.automaticallyAdjustsScrollViewInsets = false
+        addThreeSubContainer()
     }
 
     override func didReceiveMemoryWarning() {
