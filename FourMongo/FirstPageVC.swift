f39080b847d34357ccd636c8a09c3f0e3dc38676
MoMoWan <momo@www.gz.10086.cn>
2016-03-14 21:57:39 +0800

    第一次添加scrollView
    添加滚动视图约束

---
+++ FourMongo/FirstPageVC.swift
@@ -0,0 +1,50 @@
+//
+//  FirstPageVC.swift
+//  FourMongo
+//
+//  Created by 默默 on 16/3/13.
+//  Copyright © 2016年 MoMo IOS. All rights reserved.
+//
+
+import UIKit
+
+class FirstPageVC: UIViewController,UITableViewDataSource,UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension FirstPageVC{
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+        var cell = UITableViewCell()
+        return cell
+    }
+}
---
+++ Podfile
@@ -0,0 +1,15 @@
+# Uncomment this line to define a global platform for your project
+platform :ios, '8.0'
+# Uncomment this line if you're using Swift
+use_frameworks!
+
+target 'FourMongo' do
+pod 'SDCycleScrollView','~> 1.61'
+pod 'EAIntroView', '~> 2.7.0'
+pod 'SDWebImage', '~> 3.7.5'
+pod 'IQKeyboardManagerSwift', '3.3.4'
+pod 'Alamofire', '~> 3.1.5'
+pod 'SwiftyJSON', '~> 2.3.2'
+#pod 'PageMenu', '~> 1.2.9'
+end
+
