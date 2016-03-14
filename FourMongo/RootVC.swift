3827577623e5a7c70aa0df498643022483fabb21
MoMoWan <momo@www.gz.10086.cn>
2016-03-14 22:35:18 +0800

    修改了滚动第一页面、第二页面

--- FourMongo/RootVC.swift
+++ FourMongo/RootVC.swift
@@ -8,11 +8,26 @@
 
 import UIKit
 
+
+let screenWidth = UIScreen.mainScreen().bounds.width
+let screenHeight = UIScreen.mainScreen().bounds.height
+
 class RootVC: UIViewController {
+    
+    
+    //底层ScrollView容器
+    @IBOutlet weak var scrollView: UIScrollView!
+    //首页宽度
+    @IBOutlet weak var profilePageWidth: NSLayoutConstraint!
+    @IBOutlet weak var goodsPageWidth: NSLayoutConstraint!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //横向可以滚动三个页面，高度需要去除导航栏64
+        scrollView.contentSize = CGSize(width: screenWidth*3, height: screenHeight - 64)
+        profilePageWidth.constant = screenWidth
+        goodsPageWidth.constant = screenWidth
     }
 
     override func didReceiveMemoryWarning() {
