f6b9a45c0f88a4aab200d1a93b3f5dc39295bd72
MoMoWan <momo@www.gz.10086.cn>
2016-03-16 23:29:39 +0800

    添加首页的第一排和第二排容器视图

--- FourMongo/RootVC.swift
+++ FourMongo/RootVC.swift
@@ -26,9 +26,13 @@ class RootVC: UINavigationController {
         mainScrollView?.showsVerticalScrollIndicator = false
         
         //1、添加第一个页面
-        var profilePageView = UITableView(frame: CGRect(x: 0, y: 0, width: screenWidth, height: scrollViewHeight))
+        var profilePageView = UIScrollView(frame: self.view.frame)
+        profilePageView.contentSize = CGSize(width: screenWidth, height: screenHeight + 40)
+        profilePageView.backgroundColor = UIColor(red: 35, green: 57, blue: 66, alpha: 1)
         mainScrollView?.addSubview(profilePageView)
-        profilePageView.backgroundColor = UIColor.redColor() //
+        
+        var maxY = one_addSubView1(profilePageView)
+        one_addSubView2(profilePageView,startMaxY: maxY)
         
         //2、添加第二个页面（商品详细信息页面）
         var goodsInfoView = UIScrollView(frame: CGRect(x: screenWidth, y: 64, width: screenWidth, height: screenHeight - 64))
@@ -49,6 +53,71 @@ class RootVC: UINavigationController {
         self.view.addSubview(mainScrollView!)
     }
 
+    /**
+     添加第一排视图
+     
+     - parameter containerView: 父容器
+     
+     - returns: 返回最后添加完最下面一个视图的最底部的frame值
+     */
+    func one_addSubView1(containerView:UIView)->CGFloat{
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: screenWidth, height: screenWidth/5 + 20))
+        //用户头像
+        let img = UIImageView(frame: CGRect(x: 10, y: 10, width: screenWidth/5, height: screenWidth/5))
+        img.layer.cornerRadius = screenWidth/10
+        img.layer.borderWidth = CGFloat(0.5)
+        img.layer.borderColor = UIColor.redColor().CGColor
+        
+        let usrName = UILabel(frame: CGRect(x: img.frame.maxX + 10, y: 10, width: 2*screenWidth/5, height: screenWidth/5))
+        button.addSubview(img)
+        button.addSubview(usrName)
+        button.addTarget(self, action: Selector("usrInformationBtnClk"), forControlEvents: UIControlEvents.TouchUpInside)
+        containerView.addSubview(button)
+        button.backgroundColor = UIColor.yellowColor()
+        
+        return button.frame.maxY
+    }
+    /**
+     添加第二排视图
+     
+     - parameter containerView: 父容器
+     - parameter startMaxY:返回最后添加完最下面一个视图的最底部的frame值
+     */
+    func one_addSubView2(containerView:UIView,var startMaxY:CGFloat){
+        startMaxY = startMaxY + 10
+        
+        let  w = (screenWidth - 60 )/3
+        let  h = w
+        let length = CGFloat(10)
+        
+        var subView1 = UIView(frame: CGRect(x: 10, y: startMaxY, width: (screenWidth - 40 )/3, height: (screenWidth - 60 )/3))
+        subView1.backgroundColor = UIColor.redColor()
+        var subView2 = UIView(frame: CGRect(x: subView1.frame.maxX + 10, y: startMaxY, width: (screenWidth - 40 )/3, height: (screenWidth - 60 )/3))
+        subView2.backgroundColor = UIColor.redColor()
+        var subView3 = UIView(frame: CGRect(x: subView2.frame.maxX + 10, y: startMaxY, width: (screenWidth - 40 )/3, height: (screenWidth - 60 )/3))
+        subView3.backgroundColor = UIColor.redColor()
+        
+        
+        startMaxY = subView3.frame.maxY + 10
+        var subView4 = UIView(frame: CGRect(x: 10, y: startMaxY, width: (screenWidth - 40 )/3, height: (screenWidth - 60 )/3))
+        subView4.backgroundColor = UIColor.redColor()
+        var subView5 = UIView(frame: CGRect(x: subView1.frame.maxX + 10, y: startMaxY, width: (screenWidth - 40 )/3, height: (screenWidth - 60 )/3))
+        subView5.backgroundColor = UIColor.redColor()
+        var subView6 = UIView(frame: CGRect(x: subView2.frame.maxX + 10, y: startMaxY, width: (screenWidth - 40 )/3, height: (screenWidth - 60 )/3))
+        subView6.backgroundColor = UIColor.redColor()
+        
+        containerView.addSubview(subView1)
+        containerView.addSubview(subView2)
+        containerView.addSubview(subView3)
+        containerView.addSubview(subView4)
+        containerView.addSubview(subView5)
+        containerView.addSubview(subView6)
+        
+    }
+
+    func usrInformationBtnClk(){
+        NSLog("用户信息查看")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
